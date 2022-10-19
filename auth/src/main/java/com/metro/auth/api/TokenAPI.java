e311a8db4b0b4f1c147224b33bc24eb81eb89ab3
hampton-black <hampton.j.black@gmail.com>
2022-10-19 12:16:58 -0400

    Bugfix - update dependencies and imports

--- auth/src/main/java/com/metro/auth/api/TokenAPI.java
+++ auth/src/main/java/com/metro/auth/api/TokenAPI.java
@@ -14,83 +14,84 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.webage.domain.Customer;
-import com.webage.domain.CustomerFactory;
-import com.webage.domain.Token;
-import com.webage.util.JWTHelper;
+import com.metro.auth.domain.Customer;
+import com.metro.auth.domain.CustomerFactory;
+import com.metro.auth.domain.Token;
+import com.metro.auth.util.JWTHelper;
 
 @RestController
 @RequestMapping("/token")
 public class TokenAPI {
 
-	//private static Key key = AuthFilter.key;	
+	// private static Key key = AuthFilter.key;
 	public static Token appUserToken;
-	
+
 	@GetMapping
 	public String getAll() {
 		return "jwt-fake-token-asdfasdfasfa".toString();
 	}
-	
+
 	@PostMapping
-	// public ResponseEntity<?> createTokenForCustomer(@RequestBody Customer customer, HttpRequest request, UriComponentsBuilder uri) {
+	// public ResponseEntity<?> createTokenForCustomer(@RequestBody Customer
+	// customer, HttpRequest request, UriComponentsBuilder uri) {
 	public ResponseEntity<?> createTokenForCustomer(@RequestBody Customer customer) {
-		
+
 		String username = customer.getName();
 		String password = customer.getPassword();
-		
-		if (username != null && username.length() > 0 && password != null && password.length() > 0 && checkPassword(username, password)) {
+
+		if (username != null && username.length() > 0 && password != null && password.length() > 0
+				&& checkPassword(username, password)) {
 			Token token = createToken(username);
 			ResponseEntity<?> response = ResponseEntity.ok(token);
-			return response;			
+			return response;
 		}
 		// bad request
 		return (ResponseEntity<?>) ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-		
-	}
-	
-	private boolean checkPassword(String username, String password) {
-		// special case for application user
-		if(username.equals("ApiClientApp") && password.equals("secret")) {
-			return true;
-		}
-		// make call to customer service 
-		Customer cust = getCustomerByNameFromCustomerAPI(username);
-		
-		// compare name and password
-		if(cust != null && cust.getName().equals(username) && cust.getPassword().equals(password)) {
-			return true;				
-		}		
-		return false;
-		
-		// local version of the above code, gets customer from repository
-		/*
-		Iterator<Customer> customers = repo.findAll().iterator();
-		while(customers.hasNext()) {
-			Customer cust = customers.next();
-			if(cust.getName().equals(username) && cust.getPassword().equals(password)) {
-				return true;				
-			}
-		}
-		*/
-		
 
 	}
-	
+
+	private boolean checkPassword(String username, String password) {
+		// special case for application user
+		if (username.equals("ApiClientApp") && password.equals("secret")) {
+			return true;
+		}
+		// make call to customer service
+		Customer cust = getCustomerByNameFromCustomerAPI(username);
+
+		// compare name and password
+		if (cust != null && cust.getName().equals(username) && cust.getPassword().equals(password)) {
+			return true;
+		}
+		return false;
+
+		// local version of the above code, gets customer from repository
+		/*
+		 * Iterator<Customer> customers = repo.findAll().iterator();
+		 * while(customers.hasNext()) {
+		 * Customer cust = customers.next();
+		 * if(cust.getName().equals(username) && cust.getPassword().equals(password)) {
+		 * return true;
+		 * }
+		 * }
+		 */
+
+	}
+
 	public static Token getAppUserToken() {
-		if(appUserToken == null || appUserToken.getToken() == null || appUserToken.getToken().length() == 0) {
+		if (appUserToken == null || appUserToken.getToken() == null || appUserToken.getToken().length() == 0) {
 			appUserToken = createToken("ApiClientApp");
 		}
 		return appUserToken;
 	}
-	
-    private static Token createToken(String username) {
-    	String scopes = "com.webage.data.apis";
-    	// special case for application user
-    	if( username.equalsIgnoreCase("ApiClientApp")) {
-    		scopes = "com.webage.auth.apis";
-    	}
-    	String token_string = JWTHelper.createToken(scopes);
-    	
+
+	private static Token createToken(String username) {
+		String scopes = "com.webage.data.apis";
+		// special case for application user
+		if (username.equalsIgnoreCase("ApiClientApp")) {
+			scopes = "com.webage.auth.apis";
+		}
+		String token_string = JWTHelper.createToken(scopes);
+
 		/*
 		 * long fiveHoursInMillis = 1000 * 60 *60 * 5;
 		 * 
@@ -99,11 +100,10 @@ public class TokenAPI {
 		 * Date(System.currentTimeMillis() + fiveHoursInMillis)) .signWith(key)
 		 * .compact();
 		 */
-    	
-    	return new Token(token_string);
-    }
-    
-    
+
+		return new Token(token_string);
+	}
+
 	private Customer getCustomerByNameFromCustomerAPI(String username) {
 		try {
 
@@ -136,6 +136,6 @@ public class TokenAPI {
 			return null;
 		}
 
-	}  	
+	}
 
-}    
+}
